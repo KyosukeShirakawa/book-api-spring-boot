d7793f5da51a3dde97a111bfc4e2d3658359ac90
Kyosuke Shirakawa <mr.kyosuke0527@gmail.com>
2026-02-11 19:59:27 +0100

    first commit

---
+++ src/main/java/com/book/api/entity/Book.java
@@ -0,0 +1,20 @@
+package com.book.api.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Book {
+    @Id
+    private Long isbn;
+
+
+
+
+}
